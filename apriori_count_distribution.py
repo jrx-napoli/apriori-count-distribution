97ada247188c4261dcd7ffa008d827ff4c1dc5e2
Jan Rybarczyk <j.rybarczyk@stud.elka.pw.edu.pl>
2024-01-13 01:32:19 +0100

    Implemented support for parallel multiprocessing

--- apriori_count_distribution.py
+++ apriori_count_distribution.py
@@ -1,13 +1,30 @@
 from multiprocessing import Process, Manager
 from itertools import combinations
 
+def count_initial_itemsets_local(data_chunk, local_count_dist):
+    """
+    Counts local support for candidate itemsets in a data chunk
+    during the first iteration. Count is done for unique, k=1 candidate itemsets
+    available in a provided data partition.
+
+    Args:
+        data_chunk (_type_): _description_
+        local_count_dist (_type_): _description_
+    """
+    initial_candidate_itemsets = [set(item) for item in set(item for transaction in data_chunk for item in transaction)]
+    count_itemsets_local(data_chunk, initial_candidate_itemsets, local_count_dist)
+
 def count_itemsets_local(data_chunk, candidate_itemsets, local_count_dist):
-    """Count local support for candidate itemsets in a data chunk
+    """
+    Counts local support for candidate itemsets in a data chunk.
 
     Args:
         data_chunk (_type_): _description_
         candidate_itemsets (_type_): _description_
         local_count_dist (_type_): _description_
+
+    Returns:
+        void
     """
     local_support = {}
 
@@ -21,13 +38,69 @@ def count_itemsets_local(data_chunk, candidate_itemsets, local_count_dist):
     for itemset, support in local_support.items():
         local_count_dist[itemset] += support
 
+def generate_candidate_itemsets(frequent_itemsets, k):
+    """
+    Generates candidate k-itemsets from frequent (k-1)-itemsets.
+
+    Parameters:
+    - frequent_itemsets: List of frequent (k-1)-itemsets from the previous iteration.
+    - k: Size of the itemsets to generate (k-itemsets).
+
+    Returns:
+    - candidate_itemsets: List of candidate k-itemsets.
+    """
+    candidate_itemsets = []
+
+    # Generate candidate itemsets by joining (k-1)-itemsets
+    for i in range(len(frequent_itemsets)):
+        for j in range(i + 1, len(frequent_itemsets)):
+            itemset1 = frequent_itemsets[i]
+            itemset2 = frequent_itemsets[j]
+
+            # Join the itemsets if the first (k-2) elements are the same
+            if itemset1[:k-2] == itemset2[:k-2]:
+                # Create a new candidate by joining the last elements
+                new_itemset = sorted(set(itemset1).union(itemset2))
+
+                # Prune the candidate if it has an infrequent subset - apriori rule
+                if not has_infrequent_subset(new_itemset, frequent_itemsets, k-1):
+                    candidate_itemsets.append(new_itemset)
+
+    return candidate_itemsets
+
+def has_infrequent_subset(itemset, frequent_itemsets, k):
+    """
+    Checks if a candidate itemset has an infrequent subset.
+
+    Parameters:
+    - itemset: Candidate itemset.
+    - frequent_itemsets: List of frequent (k-1)-itemsets.
+    - k: Size of the subset to check.
+
+    Returns:
+    - True if the candidate has an infrequent subset, False otherwise.
+    """
+    # Generate all possible subsets of size k from the candidate itemset
+    subsets = list(combinations(itemset, k))
+
+    # Check if any subset is not frequent
+    for subset in subsets:
+        if list(subset) not in frequent_itemsets:
+            return True  # Candidate has an infrequent subset
+
+    return False  # Candidate does not have an infrequent subset
+
 def parallel_count_distribution_apriori(data, min_support, num_processes):
-    """_summary_
+    """
+    Implements the Apriori algorithm with parallel count distribution.
 
     Args:
         data (_type_): _description_
-        min_support (_type_): _description_
-        num_processes (_type_): _description_
+        min_support (int): _description_
+        num_processes (int): _description_
+
+    Returns:
+        void
     """
     # Initialize global count distribution using a shared manager
     with Manager() as manager:
@@ -35,24 +108,29 @@ def parallel_count_distribution_apriori(data, min_support, num_processes):
 
         # Initialize the dataset partitioning
         chunk_size = len(data) // num_processes
-        data_chunks = [data[i:i + chunk_size] for i in range(0, len(data), chunk_size)]
+        remaining_data = len(data) % num_processes
+
+        data_chunks = [data[i * chunk_size:(i + 1) * chunk_size] for i in range(num_processes)]
+        data_chunks[-1].extend(data[num_processes * chunk_size:num_processes * chunk_size + remaining_data])
 
         # Perform initial pass to get 1-itemset support
-        candidate_1_itemsets = [set(item) for item in set(item for transaction in data for item in transaction)]
-        local_count_dist = manager.dict({itemset: 0 for itemset in map(tuple, candidate_1_itemsets)})
+        # NOTE: In theory, in the first pass, each processor is supposed to generate 
+        # a unique, local candidate itemsets depending on its particular partition.
+        # These itemsets should only be synchronised at a later step.
+        initial_candidate_itemsets = [set(item) for item in set(item for transaction in data for item in transaction)]
+        local_count_dist = manager.dict({itemset: 0 for itemset in map(tuple, initial_candidate_itemsets)})
 
         processes = []
 
         # Parallel processing for initial count distribution
         for i in range(num_processes):
-            p = Process(target=count_itemsets_local, args=(data_chunks[i], candidate_1_itemsets, local_count_dist))
+            p = Process(target=count_initial_itemsets_local, args=(data_chunks[i], local_count_dist))
             processes.append(p)
             p.start()
 
         for p in processes:
             p.join()
 
-        # TODO: is this redundant? is global support == local support in this case?
         # Aggregate local count distributions to obtain global count distribution
         for itemset, support in local_count_dist.items():
             global_count_dist[itemset] = support
@@ -69,7 +147,7 @@ def parallel_count_distribution_apriori(data, min_support, num_processes):
             # Generate candidate itemsets for the current iteration
             candidate_itemsets = generate_candidate_itemsets(frequent_itemsets, iteration)
 
-            # Check if there are any candidate itemsets
+            # End computing if there are no candidate itemsets
             if not candidate_itemsets:
                 break
 
@@ -98,58 +176,6 @@ def parallel_count_distribution_apriori(data, min_support, num_processes):
             print(f"Frequent Itemsets (Iteration {iteration}): {frequent_itemsets}")
 
             iteration += 1
-
-def generate_candidate_itemsets(frequent_itemsets, k):
-    """
-    Generate candidate k-itemsets from frequent (k-1)-itemsets.
-
-    Parameters:
-    - frequent_itemsets: List of frequent (k-1)-itemsets from the previous iteration.
-    - k: Size of the itemsets to generate (k-itemsets).
-
-    Returns:
-    - candidate_itemsets: List of candidate k-itemsets.
-    """
-    candidate_itemsets = []
-
-    # Generate candidate itemsets by joining (k-1)-itemsets
-    for i in range(len(frequent_itemsets)):
-        for j in range(i + 1, len(frequent_itemsets)):
-            itemset1 = frequent_itemsets[i]
-            itemset2 = frequent_itemsets[j]
-
-            # Join the itemsets if the first (k-2) elements are the same
-            if itemset1[:k-2] == itemset2[:k-2]:
-                # Create a new candidate by joining the last elements
-                new_itemset = sorted(set(itemset1).union(itemset2))
-
-                # Prune the candidate if it has an infrequent subset
-                if not has_infrequent_subset(new_itemset, frequent_itemsets, k-1):
-                    candidate_itemsets.append(new_itemset)
-
-    return candidate_itemsets
-
-def has_infrequent_subset(itemset, frequent_itemsets, k):
-    """
-    Check if a candidate itemset has an infrequent subset.
-
-    Parameters:
-    - itemset: Candidate itemset.
-    - frequent_itemsets: List of frequent (k-1)-itemsets.
-    - k: Size of the subset to check.
-
-    Returns:
-    - True if the candidate has an infrequent subset, False otherwise.
-    """
-    # Generate all possible subsets of size k from the candidate itemset
-    subsets = list(combinations(itemset, k))
-
-    # Check if any subset is not frequent
-    for subset in subsets:
-        if list(subset) not in frequent_itemsets:
-            return True  # Candidate has an infrequent subset
-
-    return False  # Candidate does not have an infrequent subset
 
 # Example usage
 if __name__ == "__main__":
